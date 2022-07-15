38bd2e18008fc35f6684cc4461efa7cc8dd4dedf
Rob Kritzinger <kritz.rob@gmail.com>
2022-07-15 23:42:02 +0200

    project skeleton

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 require('dotenv').config()
 
+const fs = require('node:fs');
+const path = require('node:path');
+
+const { Client, Intents, Collection } = require('discord.js');
+const { DISC_TOKEN } = require('./config.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(DISC_TOKEN);
+
