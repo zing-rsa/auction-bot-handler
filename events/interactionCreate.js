b022bdf4ca587e593aefa18bb943a40e273db677
Rob Kritzinger <kritz.rob@gmail.com>
2022-07-17 22:57:12 +0200

    small fixes

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,6 @@
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
-        console.log('received interaction')
         if (!interaction.isCommand()) return;
 
         const command = interaction.client.commands.get(interaction.commandName);
