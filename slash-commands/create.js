9a731bdc24db1b00faa8bb4aacdb73daf37994d1
Rob <kritz.rob@gmail.com>
2022-07-28 10:22:33 +0200

    include new fields

--- slash-commands/create.js
+++ slash-commands/create.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { handle_timer_setup } = require('../timers');
 const { AUCTION_COL_NAME } = require('../config');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const { toCustomStringDate } = require('../util');
 const db = require('../mongo').db();
 const { ValidationError } = require('../errors')
@@ -22,45 +22,56 @@ module.exports = {
 		.addStringOption(option =>
 			option.setName('end-time').setDescription('The end of the auction (UTC)').setRequired(true))
 		.addStringOption(option =>
-			option.setName('image-url').setDescription('Image to display on the auction (Optional)').setRequired(false)),
+			option.setName('image-url').setDescription('Image to display on the auction (Optional)').setRequired(false))
+		.addStringOption(option =>
+			option.setName('description').setDescription('Description of the NFT being auctioned (Optional)').setRequired(false))
+		.addStringOption(option =>
+			option.setName('nft-link').setDescription('Link to the NFT (Optional)').setRequired(false))
+		.addStringOption(option =>
+			option.setName('payment-addr').setDescription('Address payment will be made to (Optional)').setRequired(false)),
 
 	async execute(interaction) {
 
 		await interaction.deferReply();
 
 		try {
-				auc_name = interaction.options.getString('name');
-				existing_channel = interaction.guild.channels.cache.find(channel => channel.name == auc_name);
-				auction_cat = interaction.guild.channels.cache.get(interaction.client.config.auction_cat);
-				start = interaction.options.get('start-time').value
-				end = interaction.options.get('end-time').value
-				const re = /\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}/
+			auc_name = interaction.options.getString('name');
+			existing_channel = interaction.guild.channels.cache.find(channel => channel.name == auc_name);
+			auction_cat = interaction.guild.channels.cache.get(interaction.client.config.auction_cat);
+			start = interaction.options.get('start-time').value
+			end = interaction.options.get('end-time').value
 
-				if (interaction.channelId != interaction.client.config.comm_channel)
-					throw new ValidationError("Can't do that here")
+			description = interaction.options.get('description') ? interaction.options.get('description').value : null;
+			nftLink = interaction.options.get('nft-link') ? interaction.options.get('nft-link').value : null;
+			paymentAddr = interaction.options.get('payment-addr') ? interaction.options.get('payment-addr').value : null;
 
-				if ([start, end].map(str => re.test(str)).includes(false)) {
-					throw new ValidationError("Those dates don't look right. Please use format: yyyy-MM-dd HH:mm:ss");
-				}
+			const re = /\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}/
 
-				try {
-					start = new Date(start + ' UTC').getTime();
-					end = new Date(end + ' UTC').getTime();
-				} catch (e) {
-					throw new ValidationError("I couldn't parse those dates. Please use format: yyyy-MM-dd HH:mm:ss");
-				}
+			if (interaction.channelId != interaction.client.config.comm_channel)
+				throw new ValidationError("Can't do that here")
 
-				if (start < Date.now())
-					throw new ValidationError("Start date must be in future");
+			if ([start, end].map(str => re.test(str)).includes(false)) {
+				throw new ValidationError("Those dates don't look right. Please use format: yyyy-MM-dd HH:mm:ss");
+			}
 
-				if (end < start)
-					throw new ValidationError("End date must be after start date");
+			try {
+				start = new Date(start + ' UTC').getTime();
+				end = new Date(end + ' UTC').getTime();
+			} catch (e) {
+				throw new ValidationError("I couldn't parse those dates. Please use format: yyyy-MM-dd HH:mm:ss");
+			}
 
-				if (existing_channel)
-					throw new ValidationError("Channel already exists");
+			if (start < Date.now())
+				throw new ValidationError("Start date must be in future");
 
-				if (!auction_cat)
-					throw new ValidationError("Can't find the auctions category");
+			if (end < start)
+				throw new ValidationError("End date must be after start date");
+
+			if (existing_channel)
+				throw new ValidationError("Channel already exists");
+
+			if (!auction_cat)
+				throw new ValidationError("Can't find the auctions category");
 
 
 			console.log('/create');
@@ -80,8 +91,8 @@ module.exports = {
 				'increment': interaction.options.get('increment').value,
 				'start': start,
 				'end': end,
-				//'start': Date.now() + 10000,
-				//'end': Date.now() + 45000,
+				// 'start': Date.now() + 10000,
+				// 'end': Date.now() + 45000,
 				'highBid': interaction.options.get('price').value,
 				'highBidId': 'none',
 				'highBidName': 'none',
@@ -103,29 +114,51 @@ module.exports = {
 
 			handle_timer_setup(auction, interaction.client);
 
+			const fields = [
+				{ name: 'Starting price:', value: auction.price + 'ADA', inline: true },
+				{ name: 'Increment:', value: auction.increment + 'ADA', inline: true },
+				{ name: '\u200B', value: '\u200B', inline: false },
+				{ name: 'Start time (UTC)', value: toCustomStringDate(new Date(auction.start)), inline: true },
+				{ name: 'End time (UTC)', value: toCustomStringDate(new Date(auction.end)), inline: true },
+				{ name: '\u200B', value: '\u200B', inline: false },
+
+				...(paymentAddr ? [{ name: 'Payment address:', value: paymentAddr, inline: true }] : []),
+
+				{ name: 'How to participate:', value: '/bid', inline: false },
+			]
+
+			let row;
+
+			if (nftLink) {
+
+				row = new MessageActionRow()
+					.addComponents(
+						new MessageButton()
+							.setLabel('View the asset here!')
+							.setURL(nftLink)
+							.setStyle('LINK')
+					);
+			}
+
 			const auctionEmbed = new MessageEmbed()
 				.setColor('0xfdbf2f')
 				.setTitle('Auction - ' + auction.name)
-				.setDescription('A new auction has been created for ' + auction.name)
-				.addFields(
-					{ name: 'Starting price:', value: auction.price + 'ADA', inline: true },
-					{ name: 'Increment:', value: auction.increment + 'ADA', inline: true },
-					{ name: '\u200B', value: '\u200B', inline: false },
-					{ name: 'Start time (UTC)', value: toCustomStringDate(new Date(auction.start)), inline: true },
-					{ name: 'End time (UTC)', value: toCustomStringDate(new Date(auction.end)), inline: true },
-					{ name: '\u200B', value: '\u200B', inline: false },
-					{ name: 'How to participate:', value: '/bid', inline: false },
-				)
+				.setDescription('A new auction has been created for ' + auction.name + `${description ? '\n\n' + description : ''}`)
+				.addFields(fields)
 				.setImage(auction.url)
 				.setTimestamp()
 				.setFooter({ text: 'Goodluck!' });
 
-			await auction_channel.send({ embeds: [auctionEmbed] });
+			if (row){
+				await auction_channel.send({ embeds: [auctionEmbed], components: [row]  });
+			} else {
+				await auction_channel.send({ embeds: [auctionEmbed] });
+			}
 
 			return await interaction.editReply('Auction created');
 
 		} catch (e) {
-			if (e instanceof ValidationError){
+			if (e instanceof ValidationError) {
 				return await interaction.editReply({ content: e.message, ephemeral: true });
 			} else {
 				console.error(e);
